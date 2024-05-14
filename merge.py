ec5bfa4cf27004829fb7dfe7a5f7a7a0d53e4cd6
Abhishek Mohite <abhishek.mohite122020@gcoeara.ac.in>
2024-05-14 16:40:13 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,33 @@
+def mergeSort(arr):
+    if(len(arr)>1):
+        mid=len(arr)//2
+
+        left_half=arr[:mid]
+        right_half=arr[mid:]
+
+        mergeSort(left_half)
+        mergeSort(right_half)
+
+        i=j=k=0
+
+        while i<len(left_half) and j<len(right_half):
+            if (left_half[i]<right_half[j]):
+                arr[k]=left_half[i]
+                i+=1
+            else:
+                arr[k]=right_half[j]
+                j+=1
+            k+=1
+        
+        while i<len(left_half):
+            arr[k]=left_half[i]
+            k+=1
+            i+=1
+        while j<len(right_half):
+            arr[k]=right_half[j]
+            j+=1
+            k+=1
+
+arr=[53,34,2,56,43,98,21]
+mergeSort(arr)
+print(arr)
